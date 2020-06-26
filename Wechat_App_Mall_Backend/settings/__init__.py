519afb2e8681cf4a3aadcc0325c8111088637f55
huzing2524 <826956010@qq.com>
2020-06-26 18:10:43 +0800

    initial commit 2020/6/26

---
+++ Wechat_App_Mall_Backend/settings/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Time    : 2020/06/26 6:08 PM
+Author  : huzing2524
+Project : Wechat_App_Mall_Backend
+File    : __init__.py.py
+Url     : https://github.com/huzing2524/Wechat_App_Mall_Backend
+"""
+
---
+++ Wechat_App_Mall_Backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Wechat_App_Mall_Backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Wechat_App_Mall_Backend.settings')
+
+application = get_wsgi_application()
